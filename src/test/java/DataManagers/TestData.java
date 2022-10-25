244dfae889bad84c339f7cb3fa7f0aca8c78e8a2
mgdncc <87605513+mgdncc@users.noreply.github.com>
2022-10-25 19:21:54 +0300

    Update test data.json file

--- src/test/java/DataManagers/TestData.java
+++ src/test/java/DataManagers/TestData.java
@@ -4,19 +4,23 @@ import aquality.selenium.core.utilities.ISettingsFile;
 import aquality.selenium.core.utilities.JsonSettingsFile;
 
 public class TestData {
-    private final static ISettingsFile settingsFile = new JsonSettingsFile("data_private.json");
+    private final static ISettingsFile settingsFile = new JsonSettingsFile("data.json");
 
-    public final static String userName = settingsFile.getValue("/userName").toString();
-    public final static String accessToken = settingsFile.getValue("/accessToken").toString();
-    public final static String password = settingsFile.getValue("/password").toString();
-    public final static String browserPackage = settingsFile.getValue("/browserPackage").toString();
-    public final static String searchFor = settingsFile.getValue("/searchFor").toString();
-    public final static String exceptedSearchFor = settingsFile.getValue("/exceptedSearchFor").toString();
-    public final static int searchResultIndex = Integer.parseInt(settingsFile.getValue("/searchResultIndex").toString());
-    public final static String exceptedRepoName = settingsFile.getValue("/exceptedRepoName").toString();
-    public final static String exceptedRepoAccess = settingsFile.getValue("/exceptedRepoAccess").toString();
-    public final static int repoIndex = Integer.parseInt(settingsFile.getValue("/repoIndex").toString());
-    public final static int screenRecordingTimeLimit = Integer.parseInt(settingsFile.getValue("/screenRecordingTimeLimit").toString());
-    public final static int randomTokenLength = Integer.parseInt(settingsFile.getValue("/randomTokenLength").toString());
+    public final static String userName = settingsFile.getValue("/secrets/userName").toString();
+    public final static String password = settingsFile.getValue("/secrets/password").toString();
+    public final static String accessToken = settingsFile.getValue("/secrets/accessToken").toString();
+
+    public final static String browserPackage = settingsFile.getValue("/loginTests/browserPackage").toString();
+    public final static int randomTokenLength = Integer.parseInt(settingsFile.getValue("/loginTests/randomTokenLength").toString());
+
+    public final static String searchFor = settingsFile.getValue("/searchTests/searchFor").toString();
+    public final static String exceptedSearchFor = settingsFile.getValue("/searchTests/exceptedSearchFor").toString();
+    public final static int searchResultIndex = Integer.parseInt(settingsFile.getValue("/searchTests/searchResultIndex").toString());
+
+    public final static String exceptedRepoName = settingsFile.getValue("/repoTests/exceptedRepoName").toString();
+    public final static String exceptedRepoAccess = settingsFile.getValue("/repoTests/exceptedRepoAccess").toString();
+    public final static int repoIndex = Integer.parseInt(settingsFile.getValue("/repoTests/repoIndex").toString());
+
+    public final static int screenRecordingTimeLimit = Integer.parseInt(settingsFile.getValue("/config/screenRecordingTimeLimit").toString());
 
 }
