a7be99d7cc1f49b095f781d77b5c34e6ec520fe3
mgdncc <87605513+mgdncc@users.noreply.github.com>
2022-10-26 18:29:03 +0300

    Add saveScreenshot to MainRecycler methods.

--- src/test/java/screens/mainRecycler/MainRecyclerScreen.java
+++ src/test/java/screens/mainRecycler/MainRecyclerScreen.java
@@ -3,6 +3,8 @@ package screens.mainRecycler;
 import aquality.appium.mobile.screens.Screen;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import utils.AllureUtils;
+import utils.DriverUtils;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ public abstract class MainRecyclerScreen extends Screen {
 
     @Step("Get Main Recycler elements.")
     public List<RecyclerElement> getElements() {
+        AllureUtils.saveScreenshot(DriverUtils.getAndroidDriver());
         return getElementFactory().findElements(getFeedElementsLocator(), RecyclerElement.class);
     }
 
